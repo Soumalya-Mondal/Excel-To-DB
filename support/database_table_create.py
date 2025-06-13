ac246f1046c425519f0b92e3edf2276726211445
Soumalya-Mondal <iamsoumalya007@hotmail.com>
2025-06-13 16:07:50 +0530

    Adding Proper Return Statement Logic Inside database_table_create.py File

--- support/database_table_create.py
+++ support/database_table_create.py
@@ -7,22 +7,22 @@ def database_table_create(env_file_path: str, table_name: str, table_create_sql:
         import os
         import psycopg2
     except Exception as error:
-        print(f'ERROR - [Database-Table-Create:S01] - {str(error)}')
+        return {'status' : 'ERROR', 'message': f'[S01] - {str(error)}'}
 
     # check if ".env" file is present:S02
     try:
         # converting ".env" file string to Path object
         env_file_path_object = Path(env_file_path)
         if (not ((env_file_path_object.exists()) and (env_file_path_object.is_file()))):
-            return {'status': 'SUCCESS', 'message': '".env" File Is Not Appropiate To Use'}
+            return {'status': 'ERROR', 'message': '".env" File Is Not Appropiate To Use'}
     except Exception as error:
-        print(f'ERROR - [Database-Table-Create:S02] - {str(error)}')
+        return {'status' : 'ERROR', 'message': f'[S02] - {str(error)}'}
 
     # load ".env" file"S03
     try:
         load_dotenv(dotenv_path = env_file_path_object)
     except Exception as error:
-        print(f'ERROR - [Database-Table-Create:S03] - {str(error)}')
+        return {'status' : 'ERROR', 'message': f'[S03] - {str(error)}'}
 
     # define database connection parameter:S04
     try:
@@ -34,9 +34,9 @@ def database_table_create(env_file_path: str, table_name: str, table_create_sql:
             "port": os.getenv('DATABASE_PORT')
         }
     except Exception as error:
-        print(f'ERROR - [Database-Table-Create:S04] - {str(error)}')
+        return {'status' : 'ERROR', 'message': f'[S04] - {str(error)}'}
 
-    # define "table_name" table create check sql query:S05
+    # define "table_name" table check sql query:S05
     try:
         table_create_check_sql = f'''
         SELECT EXISTS (
@@ -45,7 +45,7 @@ def database_table_create(env_file_path: str, table_name: str, table_create_sql:
             AND table_name = '{table_name}'
         );'''
     except Exception as error:
-        print(f'ERROR - [Database-Table-Create:S05] - {str(error)}')
+        return {'status' : 'ERROR', 'message': f'[S05] - {str(error)}'}
 
     # check if "table_name" table is already present:S06
     try:
@@ -56,7 +56,7 @@ def database_table_create(env_file_path: str, table_name: str, table_create_sql:
                 if table_already_present_result:
                     return {'status': 'SUCCESS', 'message': f'"{table_name}" Already Present Inside Database'}
     except Exception as error:
-        print(f'ERROR - [Database-Table-Create:S06] - {str(error)}')
+        return {'status' : 'ERROR', 'message': f'[S06] - {str(error)}'}
 
     # connect database and execute table create query:S07
     try:
@@ -64,7 +64,7 @@ def database_table_create(env_file_path: str, table_name: str, table_create_sql:
             with database_connection.cursor() as database_cursor:
                 database_cursor.execute(table_create_sql)
     except Exception as error:
-        print(f'ERROR - [Database-Table-Create:S07] - {str(error)}')
+        return {'status' : 'ERROR', 'message': f'[S07] - {str(error)}'}
 
     # check if "account_file_list" table is created:S08
     try:
@@ -77,4 +77,4 @@ def database_table_create(env_file_path: str, table_name: str, table_create_sql:
                 else:
                     return {'status': 'ERROR', 'message': f'"{table_name}" Not Created Inside Database'}
     except Exception as error:
-        print(f'ERROR - [Database-Table-Create:S08] - {str(error)}')
+        return {'status' : 'ERROR', 'message': f'[S08] - {str(error)}'}
